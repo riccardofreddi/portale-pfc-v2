dee8e02d02303938dd17541e85d223567d83a7ae
riccardofreddi <riccardofreddi6@gmail.com>
2026-07-24 18:43:56 +0200

    Fix: prisma.config.ts without hardcoded password + postgresql provider

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: path.join('prisma', 'schema.prisma'),
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   datasource: {
     url: process.env.DATABASE_URL ?? 'file:./db/custom.db',
   },
