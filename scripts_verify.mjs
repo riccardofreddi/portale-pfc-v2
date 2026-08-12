ef33312419fe3d0a7329917ba36ab56a1acc9378
riccardofreddi <riccardofreddi6@gmail.com>
2026-08-12 19:00:50 +0200

    scadenze funzionanti

--- scripts_verify.mjs
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from '@prisma/client'
-
-const directUrl = process.env.DATABASE_URL.replace(':6543', ':5432')
-const db = new PrismaClient({ datasources: { db: { url: directUrl } } })
-
-const r = await db.notification.deleteMany({ where: { type: 'scadenza' } })
-console.log('notifiche scadenza rimosse:', r.count)
-await db.$disconnect()
